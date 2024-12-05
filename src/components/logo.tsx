b4a7cb39f3641774e3a3c595a73c6ce6dddadde1
timh <timhansen.se@gmail.com>
2024-12-05 18:53:55 +0100

    feat: new logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,15 +1,22 @@
 import Image from 'next/image';
+import { Coiny } from 'next/font/google';
+
+const coinFont = Coiny({ subsets: ['latin'], weight: ['400'] });
 
 export default function Logo() {
   return (
-    <header className='absolute top-4 left-4'>
+    <header className='absolute top-4 left-4 flex items-center space-x-1'>
+      {/* Logo Image */}
       <Image
-        src='/images/logo.png'
+        src='/images/beer-logo.png'
         alt='SipHappns Logo'
-        width={80}
-        height={80}
+        width={45}
+        height={45}
         className='object-contain'
       />
+
+      {/* Logo Text */}
+      <h1 className={`text-lg font-bold ${coinFont.className}`}>SipHappns</h1>
     </header>
   );
 }
